7cc1aabf2ffc5cf48489b896c20c6743f29c1cfb
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-03-06 06:39:34 +1100

    Download books from tululu.org

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+import os
 import requests
 import urllib3
 
-from urllib.parse import urlparse
+
+def download_file(file_url, payload, file_path):
+    response = requests.get(file_url, params=payload, verify=False)
+    response.raise_for_status()
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
 
 
-book_url = 'https://tululu.org/txt.php?id=32168'
-urllib3.disable_warnings()
-file_name = f'{urlparse(book_url).query.replace("=", "")}.txt'
+def main():
+    urllib3.disable_warnings()
+    book_url = 'https://tululu.org/txt.php'
+    folder = 'books'
+    os.makedirs(folder, exist_ok=True)
+    for id in range(1, 11):
+        payload = {"id": id}
+        file_name = f'id{id}.txt'
+        file_path = os.path.join(os.getcwd(), folder, file_name)
+        download_file(book_url, payload, file_path)
 
-response = requests.get(book_url, verify=False)
-response.raise_for_status()
-with open(file_name, 'wb') as file:
-    file.write(response.content)
+
+if __name__ == '__main__':
+    main()
