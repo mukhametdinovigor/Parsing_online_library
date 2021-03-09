bd30868ed8c8c05cf92253b035630a263bd8407e
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-03-09 21:56:19 +1100

    Download comments

--- main.py
+++ main.py
@@ -33,7 +33,11 @@ def get_book_attributes(book_id, payload):
     book_title = soup.find('h1').text.split('::')[0].strip()
     image_name = soup.find('div', class_='bookimage').find('img').attrs.get('src')
     image_url = urljoin('https://tululu.org', image_name)
-    return book_title, image_url
+    book_comments = []
+    comments = soup.find_all('div', class_='texts')
+    for comment in comments:
+        book_comments.append(comment.contents[4].text)
+    return book_title, image_url, book_comments
 
 
 def main():
@@ -49,11 +53,13 @@ def main():
             check_for_redirect(response)
         except requests.exceptions.HTTPError:
             continue
-        book_title, image_url = get_book_attributes(book_id, payload)
+        book_title, image_url, book_comments = get_book_attributes(book_id, payload)
         txt_file_name = f'{book_id}. {book_title}.txt'
         image_file_name = unquote(os.path.split(urlparse(image_url).path)[1])
         download_file(txt_file_name, book_folder, response.content)
         download_cover(image_url, image_file_name, images_folder)
+        print(book_title)
+        print(*book_comments)
 
 
 if __name__ == '__main__':
