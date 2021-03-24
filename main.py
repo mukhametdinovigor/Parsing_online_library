8b42ceaf9e89528a814b16255dd3b09f9195a03c
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-03-25 01:45:58 +1100

    Use selectors instead find() methods

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def create_args_parser():
     return args
 
 
-def check_for_error(book_url, payload):
+def check_for_errors(book_url, payload):
     response = requests.get(book_url, params=payload, verify=False)
     response.raise_for_status()
     if len(response.history):
@@ -32,8 +32,8 @@ def get_scifi_books_page_html(url):
 
 def get_book_ids(books_page):
     soup = BeautifulSoup(books_page, 'lxml')
-    book_blocks = soup.find_all('table', class_='d_book')
-    books_ids = [book_block.find('a').attrs.get('href')[2:-1] for book_block in book_blocks]
+    id_selector = '.d_book a[href^="/b"][title^="Бесплатная"]'
+    books_ids = [book_block.attrs.get('href')[2:-1]for book_block in soup.select(id_selector)]
     return books_ids
 
 
@@ -46,17 +46,18 @@ def get_book_page_html(book_id, payload):
 
 def parse_book_page(book_description):
     soup = BeautifulSoup(book_description, 'lxml')
-    book_title = soup.find('h1').text.split('::')[0].strip()
-    book_author = soup.find('h1').text.split('::')[1].strip()
-    image_name = soup.find('div', class_='bookimage').find('img').attrs.get('src')
+    title_author_selector = 'h1'
+    image_selector = '.bookimage img'
+    comments_selector = '.texts .black'
+    genres_selector = 'span.d_book a'
+    book_title, book_author = soup.select(title_author_selector)[0].text.split('::')
+    image_name = soup.select(image_selector)[0].attrs.get('src')
     image_url = urljoin('https://tululu.org', image_name)
-    comments = soup.find_all('div', class_='texts')
-    book_comments = [comment.contents[4].text for comment in comments]
-    genres = soup.find('span', class_='d_book').find_all('a')
-    book_genres = [genre.text for genre in genres]
+    book_comments = [comment.text for comment in soup.select(comments_selector)]
+    book_genres = [genre.text for genre in soup.select(genres_selector)]
     book_attributes = {
-        'book_title': book_title,
-        'book_author': book_author,
+        'book_title': book_title.strip(),
+        'book_author': book_author.strip(),
         'image_url': image_url,
         'book_comments': book_comments,
         'book_genres': book_genres
@@ -78,11 +79,13 @@ def download_book(filename, folder, book_url, payload):
     book_path = download_file(filename, folder, response.text, 'w')
     return book_path
 
+
 def download_cover(image_url, filename, folder):
     response = requests.get(image_url, verify=False)
     response.raise_for_status()
     img_src = download_file(filename, folder, response.content, 'wb')
     return img_src
+
 
 def get_books_json(book_attributes, img_src, book_path):
     books = {
@@ -97,7 +100,6 @@ def get_books_json(book_attributes, img_src, book_path):
         json.dump(books, my_file, ensure_ascii=False, indent=4)
 
 
-
 def main():
     urllib3.disable_warnings()
     args = create_args_parser()
@@ -105,14 +107,14 @@ def main():
     book_url = 'https://tululu.org/txt.php'
     book_folder = 'books'
     images_folder = 'images'
-    for page_number in range(1, 5):
+    for page_number in range(1, 2):
         scifi_books_page_url = urljoin(scifi_books_url, str(page_number))
         books_page = get_scifi_books_page_html(scifi_books_page_url)
         book_ids = get_book_ids(books_page)
         for book_id in book_ids:
             payload = {"id": book_id}
             try:
-                check_for_error(book_url, payload)
+                check_for_errors(book_url, payload)
                 book_description = get_book_page_html(book_id, payload)
                 book_attributes = parse_book_page(book_description)
                 book_title = book_attributes.get('book_title')
