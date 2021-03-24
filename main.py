faa42c2f2686071c5689ac04455852a64eecd468
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-03-25 00:14:54 +1100

    Download books from all pages

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 from urllib.parse import urljoin, urlparse, unquote
 
@@ -21,6 +22,19 @@ def check_for_error(book_url, payload):
     response.raise_for_status()
     if len(response.history):
         raise requests.HTTPError()
+
+
+def get_scifi_books_page_html(url):
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+    return response.text
+
+
+def get_book_ids(books_page):
+    soup = BeautifulSoup(books_page, 'lxml')
+    book_blocks = soup.find_all('table', class_='d_book')
+    books_ids = [book_block.find('a').attrs.get('href')[2:-1] for book_block in book_blocks]
+    return books_ids
 
 
 def get_book_page_html(book_id, payload):
@@ -55,40 +69,61 @@ def download_file(filename, folder, response_file, mode):
     file_path = sanitize_filepath(os.path.join(folder, sanitize_filename(filename)))
     with open(file_path, mode) as file:
         file.write(response_file)
+    return file_path
 
 
 def download_book(filename, folder, book_url, payload):
     response = requests.get(book_url, params=payload, verify=False)
     response.raise_for_status()
-    download_file(filename, folder, response.text, 'w')
-
+    book_path = download_file(filename, folder, response.text, 'w')
+    return book_path
 
 def download_cover(image_url, filename, folder):
     response = requests.get(image_url, verify=False)
     response.raise_for_status()
-    download_file(filename, folder, response.content, 'wb')
+    img_src = download_file(filename, folder, response.content, 'wb')
+    return img_src
+
+def get_books_json(book_attributes, img_src, book_path):
+    books = {
+        'title': book_attributes.get('book_title'),
+        'author': book_attributes.get('book_author'),
+        'img_src': img_src,
+        'book_path': book_path,
+        'comments': book_attributes.get('book_comments'),
+        'genres': book_attributes.get('book_genres')
+    }
+    with open("books.json", "a", encoding='utf8') as my_file:
+        json.dump(books, my_file, ensure_ascii=False, indent=4)
+
 
 
 def main():
     urllib3.disable_warnings()
     args = create_args_parser()
+    scifi_books_url = 'https://tululu.org/l55/'
     book_url = 'https://tululu.org/txt.php'
     book_folder = 'books'
     images_folder = 'images'
-    for book_id in range(args.start_id, args.end_id + 1):
-        payload = {"id": book_id}
-        try:
-            check_for_error(book_url, payload)
-            book_description = get_book_page_html(book_id, payload)
-            book_attributes = parse_book_page(book_description)
-            book_title = book_attributes.get('book_title')
-            image_url = book_attributes.get('image_url')
-            txt_file_name = f'{book_id}. {book_title}.txt'
-            image_file_name = unquote(os.path.split(urlparse(image_url).path)[1])
-            download_book(txt_file_name, book_folder, book_url, payload)
-            download_cover(image_url, image_file_name, images_folder)   
-        except requests.exceptions.HTTPError:
-            continue
+    for page_number in range(1, 5):
+        scifi_books_page_url = urljoin(scifi_books_url, str(page_number))
+        books_page = get_scifi_books_page_html(scifi_books_page_url)
+        book_ids = get_book_ids(books_page)
+        for book_id in book_ids:
+            payload = {"id": book_id}
+            try:
+                check_for_error(book_url, payload)
+                book_description = get_book_page_html(book_id, payload)
+                book_attributes = parse_book_page(book_description)
+                book_title = book_attributes.get('book_title')
+                image_url = book_attributes.get('image_url')
+                txt_file_name = f'{book_id}.{book_title}.txt'
+                image_file_name = unquote(os.path.split(urlparse(image_url).path)[1])
+                book_path = download_book(txt_file_name, book_folder, book_url, payload)
+                img_src = download_cover(image_url, image_file_name, images_folder)
+                get_books_json(book_attributes, img_src, book_path)
+            except requests.exceptions.HTTPError:
+                continue
 
 
 if __name__ == '__main__':
